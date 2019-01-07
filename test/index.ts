9c06d350757300c894251f8c84a9942eaa8089de
samrg472 <samrg472@gmail.com>
2019-01-07 14:47:44 -0800

    Fix tests and add config sample

--- test/index.ts
+++ test/index.ts
@@ -1,16 +1,13 @@
 // Load the test environment before anything else
-import 'reflect-metadata';
-
 import * as process from 'process';
 process.env.NODE_ENV = 'TEST';
 process.env.TYPEORM_OVERRIDES = '../ormconfig.test.js';
 
 import { setConfig } from '../src/config';
 setConfig({
-  steem_net: undefined as any,
-  steem_settings: undefined as any,
-  steem_connect: undefined as any,
-  https_server: undefined as any
+  steem_net: undefined!,
+  steem_settings: undefined!,
+  steem_connect: undefined!
 });
 
 describe('Lock', () => {
