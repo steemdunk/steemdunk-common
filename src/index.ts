212f422a0c31330f922e353234d78c5a1de47e3f
samrg472 <samrg472@gmail.com>
2019-02-11 13:44:09 -0800

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,2 @@
 export * from './logger-factory';
 export * from './payment';
-export * from './config';
-export * from './util';
-export * from './lock';
-export * from './sc2';
--- test/index.ts
+++ test/index.ts
@@ -1,8 +1,2 @@
 // Load the test environment before anything else
-import * as process from 'process';
 process.env.NODE_ENV = 'TEST';
-process.env.TYPEORM_OVERRIDES = '../ormconfig.test.js';
-
-describe('Lock', () => {
-  require('./lock');
-});
